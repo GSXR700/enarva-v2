211ec7b84209e98b41aae627281ea58056c84888
GSXR700 <vantezsteve@gmail.com>
2025-09-08 14:04:15 +0100

    Fixed errors v7

--- app/api/auth/[...nextauth]/route.ts
+++
@@ -1,9 +0,0 @@
-import { authOptions } from '@/lib/auth'
-
-async function handler(req: Request) {
-  // @ts-ignore - NextAuth type issue in Next.js 15
-  const NextAuth = require('next-auth').default || require('next-auth')
-  return NextAuth(authOptions)(req)
-}
-
-export { handler as GET, handler as POST }
